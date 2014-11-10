f152d37f4906ed52b223f6f0fb5536aa15c34a7e
Luke Marsden <luke@clusterhq.com>
2014-11-10 05:41:24 +0000

    add some logging, mount the right volumes

--- run.py
+++ run.py
@@ -8,16 +8,18 @@ Usage: run.py <number-of-containers>
 import os, sys
 
 # kill all containers
+print 'killing containers...'
 os.system("docker rm -f $(docker ps -a -q)")
 
 # ord('z')
 
+print 'starting up', sys.argv[1], 'of them...'
 for i in range(ord('b'), ord('b') + int(sys.argv[1])):
     x = chr(i)
     print 'doing', x
     path = "/big/%s" % (x,)
     os.system("rm -rf %s/*" % (path,))
-    os.system("docker run -v %s:/data dockerfile/percona mysql_install_db" % (path,))
-    os.system("docker run -d -v %s:/data dockerfile/percona" % (path,))
+    os.system("docker run -v %s:/var/lib/mysql dockerfile/percona mysql_install_db" % (path,))
+    os.system("docker run -d -v %s:/var/lib/mysql dockerfile/percona" % (path,))
 
 
