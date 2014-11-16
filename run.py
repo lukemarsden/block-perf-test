91583af7fb8405238179d1b220a4a27dee8f9aeb
Luke Marsden <luke@hybridcluster.com>
2014-11-16 11:46:01 -0800

    Add a way to run benchmarks faster if you want to reuse existing containers and their loaded data

--- run.py
+++ run.py
@@ -4,43 +4,50 @@ usage = """
 Starts mysql containers on a set of volumes: /big/[b-z], wiping them out first,
 and running mysql_install_db.
 
-Usage: run.py <number-of-containers> <warehouses-per-benchmark> <test-time-in-seconds>
+Usage: run.py <number-of-containers> <warehouses-per-benchmark> <test-time-in-seconds> <do-load-step:1=yes,0=no>
+
+Set do-load-step to 0 if you have already created and loaded the databases.
 """
 
 import os, sys
 from twisted.internet import reactor, defer, utils, task
 from twisted.python import log
 
-if len(sys.argv) < 4:
+if len(sys.argv) < 5:
     print usage
     raise SystemExit()
 
 CONCURRENCY = int(sys.argv[1])
 WAREHOUSES = int(sys.argv[2]) # 2
 TEST_TIME = int(sys.argv[3]) # 120
+DO_LOAD_STEP = int(sys.argv[4])
 
 print 'disabling security'
 os.system("setenforce 0")
-# kill all containers
-print 'killing containers...'
-os.system("docker rm -f $(docker ps -a -q)")
 
 concurrent = []
-print 'starting up', CONCURRENCY, 'of them...'
 for i in range(ord('b'), ord('b') + CONCURRENCY):
     concurrent.append(i)
-    x = chr(i)
-    print 'doing', x
-    path = "/big/%s" % (x,)
-    hostPort = 4000 + i
-    print 'allocating hostPort', hostPort
-    os.system("rm -rf %s/*" % (path,))
-    cmd = (("""docker run -v %s:/var/lib/mysql dockerfile/percona sh -c """
-            """'mysql_install_db && mysqld_safe & mysqladmin --silent --wait=30 ping || exit 1 &&"""
-            """ mysql -e "GRANT ALL PRIVILEGES ON *.* TO \\\"root\\\"@\\\"%%\\\" WITH GRANT OPTION;"'""") % (path,))
-    print 'running', cmd
-    os.system(cmd)
-    os.system("docker run -d -v %s:/var/lib/mysql -v /root/block-perf-test/conf:/etc/mysql --publish=%d:3306 --name=mysql-%d-%s dockerfile/percona" % (path, hostPort, i, x))
+
+if DO_LOAD_STEP:
+    # kill all containers
+    print 'killing containers...'
+    os.system("docker rm -f $(docker ps -a -q)")
+
+    print 'starting up', CONCURRENCY, 'of them...'
+    for i in concurrent:
+        x = chr(i)
+        print 'doing', x
+        path = "/big/%s" % (x,)
+        hostPort = 4000 + i
+        print 'allocating hostPort', hostPort
+        os.system("rm -rf %s/*" % (path,))
+        cmd = (("""docker run -v %s:/var/lib/mysql dockerfile/percona sh -c """
+                """'mysql_install_db && mysqld_safe & mysqladmin --silent --wait=30 ping || exit 1 &&"""
+                """ mysql -e "GRANT ALL PRIVILEGES ON *.* TO \\\"root\\\"@\\\"%%\\\" WITH GRANT OPTION;"'""") % (path,))
+        print 'running', cmd
+        os.system(cmd)
+        os.system("docker run -d -v %s:/var/lib/mysql -v /root/block-perf-test/conf:/etc/mysql --publish=%d:3306 --name=mysql-%d-%s dockerfile/percona" % (path, hostPort, i, x))
 
 def printIt(result):
     print result
@@ -66,9 +73,10 @@ def inject():
         d.addCallback(lambda ignored, hp: run("bash", ["-c",
             "mysql -h localhost -P %d --protocol=tcp tpcc1000 < /root/tpcc-mysql/add_fkey_idx.sql" % (hp,)]), hp=hostPort)
         d.addCallback(printIt)
-        d.addCallback(lambda ignored, hp: run('/root/tpcc-mysql/tpcc_load',
-            ["127.0.0.1:%d" % (hp,), "tpcc1000", "root", "", "%d" % (WAREHOUSES,)]), hp=hostPort)
-        d.addCallback(printIt)
+        if DO_LOAD_STEP:
+            d.addCallback(lambda ignored, hp: run('/root/tpcc-mysql/tpcc_load',
+                ["127.0.0.1:%d" % (hp,), "tpcc1000", "root", "", "%d" % (WAREHOUSES,)]), hp=hostPort)
+            d.addCallback(printIt)
         d.addErrback(log.err, 'failed while creating database %d' % (i,))
         dlist.append(d)
     return defer.gatherResults(dlist)
