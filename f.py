50060622a9624b25099470ba24235d4d917f2488
Luke Marsden <luke@hybridcluster.com>
2014-11-15 23:18:10 -0800

    replace

--- f.py
+++ f.py
@@ -5,11 +5,8 @@ def printIt(result):
     print result
 
 def foo():
-    f = open('f', 'w')
-    f.write('f')
-    f.close()
-
-    d = utils.getProcessOutput("bash", ["-c", "cat < f"])
+    d = utils.getProcessOutput("bash", ["-c",
+        "mysql -h localhost -P %d --protocol=tcp tpcc1000 < /root/tpcc-mysql/create_table.sql" % (1,)], errortoo=True)
     d.addErrback(log.err, 'oops')
     d.addCallback(printIt)
     d.addBoth(lambda ignored: reactor.stop())
